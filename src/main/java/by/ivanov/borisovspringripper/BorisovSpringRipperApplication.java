ef1236e3029d022cfffaa0be9704439e0f08900d
ivanivanov <ISIvanov@lanit.ru>
2022-09-13 09:59:21 +0300

    Initial commit

---
+++ src/main/java/by/ivanov/borisovspringripper/BorisovSpringRipperApplication.java
@@ -0,0 +1,13 @@
+package by.ivanov.borisovspringripper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorisovSpringRipperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BorisovSpringRipperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/ivanov/borisovspringripper/BorisovSpringRipperApplicationTests.java
@@ -0,0 +1,13 @@
+package by.ivanov.borisovspringripper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BorisovSpringRipperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
